db4cac684c22128e58f6af15111508f08c6ac169
Esotopo21 <cristianoburla@gmail.com>
2020-10-26 11:38:33 +0100

    ADD WebAppConfig.java

---
+++ chatapplicationserver/src/main/java/it/burlac/chatapplication/configuration/WebAppConfig.java
@@ -0,0 +1,16 @@
+package it.burlac.chatapplication.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebAppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://95.111.229.45:8081")
+                .allowedMethods("GET");
+    }
+}
