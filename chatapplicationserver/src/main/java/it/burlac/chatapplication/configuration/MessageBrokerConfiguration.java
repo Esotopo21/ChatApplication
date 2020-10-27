0a270d86dabf9a425aff5b8c551163a43826a4f9
Esotopo21 <cristianoburla@gmail.com>
2020-10-27 12:26:18 +0100

    UPGRADE springboot version FIX MessageBrokerConfiguration

--- chatapplicationserver/src/main/java/it/burlac/chatapplication/configuration/MessageBrokerConfiguration.java
+++ chatapplicationserver/src/main/java/it/burlac/chatapplication/configuration/MessageBrokerConfiguration.java
@@ -3,15 +3,12 @@ package it.burlac.chatapplication.configuration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 import org.springframework.web.socket.server.HandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+public class MessageBrokerConfiguration implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
@@ -23,4 +20,5 @@ public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfig
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
+
 }
